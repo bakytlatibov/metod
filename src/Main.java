343ce751c5b760a2f718a4e8a1be1fa3ab214e41
bakytlatibov <bakytlatibov3@gmail.com>
2022-12-28 21:45:33 +0300

    ciommit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        int[] arr = new int[]{23,67,45,667,2,3,3,34,4,556,789,};
+        System.out.println(mass(arr));
+
+//        Scanner sc=new Scanner(System.in);
+//        int san= sc.nextInt();
+//        if (san==1){
+//            hello();
+//        }else if(san==2){
+//            bye();
+//        }
+//
+//
+//
+//
+//    }
+//
+//    static void hello(){
+//        System.out.println("hello");
+//
+//    }
+//    static void  bye(){
+//        System.out.println("Bye"{
+
+
+//   static String san(int a) {
+//       if (a % 2 == 0) {
+//           return "jup san";
+//       } else {
+//           return "tak san";
+
+
+     }static int san(int a) {
+    int summa=0;
+        for (int i = 0; i <= a; i++) {
+          summa+=i;
+        }
+   return summa;
+
+    }static int mass(int []arr) {
+
+        int max=arr[4];
+        for (int i = 0; i > arr.length; i++) {
+            max=Math.min(max,arr[i]);
+
+
+        }
+  return max;
+
+
+    }}
